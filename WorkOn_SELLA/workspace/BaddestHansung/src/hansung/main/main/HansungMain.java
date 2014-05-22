eb451c2f61a54a4c8e774d1b3dee1ed48a28ac25
Sella <happyvirussella@gmail.com>
2014-05-23 07:09:46 +0900

    Changed src, res files
    HansungMain 액티비티와 CampusMain액티비티 및 레이아웃 파일들에 내용 추가

--- WorkOn_SELLA/workspace/BaddestHansung/src/hansung/main/main/HansungMain.java
+++ WorkOn_SELLA/workspace/BaddestHansung/src/hansung/main/main/HansungMain.java
@@ -2,6 +2,8 @@ package hansung.main.main;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class HansungMain extends Activity {
     @Override
@@ -9,6 +11,17 @@ public class HansungMain extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         
-        
+        //CampusMap Button
+        Button bt_CampusMap = (Button)findViewById(R.id.bt_CampusMap);
+		bt_CampusMap.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				
+				
+			}
+		});
+		
+		//Timetable Button
+		Button bt_Timetable = (Button)findViewById(R.id.bt_Timetable);
     }
 }
