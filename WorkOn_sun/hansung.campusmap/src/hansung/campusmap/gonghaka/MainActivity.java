e55c23e512e4037a823412fc46c96a2add67475d
YSSung <sun3141@nate.com>
2014-05-31 18:31:44 +0900

    updated .xml

--- WorkOn_sun/hansung.campusmap/src/hansung/campusmap/gonghaka/MainActivity.java
+++ WorkOn_sun/hansung.campusmap/src/hansung/campusmap/gonghaka/MainActivity.java
@@ -9,7 +9,7 @@ public class MainActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.campus_gonghakab1);
+		setContentView(R.layout.campus_gonghaka1);
 	}
 
 	@Override
